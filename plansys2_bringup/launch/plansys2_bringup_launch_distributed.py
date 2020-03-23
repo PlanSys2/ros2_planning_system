22677524c7d42871b295e64266c188d770e0f974
Francisco Martin Rico <fmrico@gmail.com>
2020-03-23 12:48:28 +0100

    Linting
    Signed-off-by: Francisco Martin Rico <fmrico@gmail.com>

--- plansys2_bringup/launch/plansys2_bringup_launch_distributed.py
+++ plansys2_bringup/launch/plansys2_bringup_launch_distributed.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
