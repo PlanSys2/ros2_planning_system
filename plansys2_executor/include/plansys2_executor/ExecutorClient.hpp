2f6c390bda00db83681e22441f268c2f0d20070b
Josh Zapf <jjzapf@spawar.navy.mil>
2020-12-14 15:05:42 -0800

    NIWCMM-49: Fixing reorder warning.

--- plansys2_executor/include/plansys2_executor/ExecutorClient.hpp
+++ plansys2_executor/include/plansys2_executor/ExecutorClient.hpp
@@ -42,13 +42,12 @@ public:
   boost::optional<ExecutePlan::Result> getResult();
 
 private:
+  rclcpp::Node::SharedPtr node_;
   rclcpp_action::Client<ExecutePlan>::SharedPtr execute_plan_client_ptr_;
 
   ExecutePlan::Feedback feedback_;
   ExecutePlan::Result result_;
   bool finished_;
-
-  rclcpp::Node::SharedPtr node_;
 
   void feedback_callback(
     GoalHandleExecutePlan::SharedPtr,
