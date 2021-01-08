a0995a7fc0552d07b9bba91b006a8cce2d3d8fa8
Josh Zapf <jjzapf@spawar.navy.mil>
2021-01-08 14:55:22 -0800

    NIWCMM-57: No need to explicitly define PDDL constructs.

--- plansys2_pddl_parser/src/plansys2_pddl_parser/Tree.cpp
+++ plansys2_pddl_parser/src/plansys2_pddl_parser/Tree.cpp
@@ -53,9 +53,9 @@ std::string getReducedString(const std::string & expr)
   return ret;
 }
 
-NodeType getNodeType(const std::string & expr)
+NodeType getNodeType(const std::string & expr, NodeType default_node_type)
 {
-  NodeType node_type = UNKNOWN;
+  NodeType node_type = default_node_type;
 
   std::smatch match;
   int first = std::numeric_limits<int>::max();
@@ -81,36 +81,24 @@ NodeType getNodeType(const std::string & expr)
     }
   }
 
-  if (std::regex_search(expr, match, std::regex("\\(predicate"))) {
-    if (static_cast<int>(match.position()) < first) {
-      first = static_cast<int>(match.position());
-      node_type = PREDICATE;
-    }
-  }
-
-  // NOTE: "function_modifier" must precede "function"
-  if (std::regex_search(expr, match, std::regex("\\(function_modifier"))) {
-    if (static_cast<int>(match.position()) < first) {
-      first = static_cast<int>(match.position());
+  std::tuple<FunModType, int> modifier_search_result = getFunMod(expr);
+  if (std::get<0>(modifier_search_result) != UNKNOWN_FUN_MOD_TYPE) {
+    if (std::get<1>(modifier_search_result) < first) {
+      first = std::get<1>(modifier_search_result);
       node_type = FUNCTION_MODIFIER;
     }
   }
 
-  if (std::regex_search(expr, match, std::regex("\\(function"))) {
-    if (static_cast<int>(match.position()) < first) {
-      first = static_cast<int>(match.position());
-      node_type = FUNCTION;
-    }
-  }
-
-  if (std::regex_search(expr, match, std::regex("\\(expression"))) {
-    if (static_cast<int>(match.position()) < first) {
-      first = static_cast<int>(match.position());
+  std::tuple<ExprType, int> expression_search_result = getExpr(expr);
+  if (std::get<0>(expression_search_result) != UNKNOWN_EXPR_TYPE) {
+    if (std::get<1>(expression_search_result) < first) {
+      first = std::get<1>(expression_search_result);
       node_type = EXPRESSION;
     }
   }
 
-  if (std::regex_search(expr, match, std::regex("\\(number"))) {
+  std::regex num_regexp("[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)");
+  if (std::regex_search(expr, match, num_regexp)) {
     if (static_cast<int>(match.position()) < first) {
       first = static_cast<int>(match.position());
       node_type = NUMBER;
@@ -121,6 +109,12 @@ NodeType getNodeType(const std::string & expr)
 }
 
 ExprType getExprType(const std::string & input)
+{
+  std::tuple<ExprType, int> result = getExpr(input);
+  return std::get<0>(result);
+}
+
+std::tuple<ExprType, int> getExpr(const std::string & input)
 {
   ExprType expr_type = UNKNOWN_EXPR_TYPE;
 
@@ -169,10 +163,16 @@ ExprType getExprType(const std::string & input)
     }
   }
 
-  return expr_type;
+  return std::make_tuple(expr_type, first);
 }
 
 FunModType getFunModType(const std::string & input)
+{
+  std::tuple<FunModType, int> result = getFunMod(input);
+  return std::get<0>(result);
+}
+
+std::tuple<FunModType, int> getFunMod(const std::string & input)
 {
   FunModType fun_mod_type = UNKNOWN_FUN_MOD_TYPE;
 
@@ -214,7 +214,7 @@ FunModType getFunModType(const std::string & input)
     }
   }
 
-  return fun_mod_type;
+  return std::make_tuple(fun_mod_type, first);
 }
 
 int getParenthesis(const std::string & wexpr, int start)
@@ -259,23 +259,19 @@ std::vector<std::string> getSubExpr(const std::string & expr)
       if (match.position() < first) {
         ret.push_back(wexpr.substr(match.position(), match.length()));
         wexpr.erase(wexpr.begin(), wexpr.begin() + match.position() + match.length());
-      }
-      else {
+      } else {
         int last = getParenthesis(wexpr, first);
         ret.push_back(wexpr.substr(first, last - first + 1));
         wexpr.erase(wexpr.begin(), wexpr.begin() + last + 1);
       }
-    }
-    else if (found_num) {
+    } else if (found_num) {
       ret.push_back(wexpr.substr(match.position(), match.length()));
       wexpr.erase(wexpr.begin(), wexpr.begin() + match.position() + match.length());
-    }
-    else if (first != std::string::npos) {
+    } else if (first != std::string::npos) {
       int last = getParenthesis(wexpr, first);
       ret.push_back(wexpr.substr(first, last - first + 1));
       wexpr.erase(wexpr.begin(), wexpr.begin() + last + 1);
-    }
-    else {
+    } else {
       break;
     }
   }
@@ -359,13 +355,29 @@ std::vector<parser::pddl::tree::Param> getFunctionParams(const std::string & exp
   return ret;
 }
 
-std::shared_ptr<TreeNode> get_tree_node(const std::string & expr,
-                                        const std::string & construct)
+std::shared_ptr<TreeNode> get_tree_node(const std::string & expr, NodeType parent)
 {
   std::string wexpr = getReducedString(expr);
-  std::string wconstruct = getReducedString(construct);
 
-  NodeType node_type = getNodeType(wconstruct);
+  NodeType default_node_type = UNKNOWN_NODE_TYPE;
+  switch (parent) {
+    case AND:
+      default_node_type = PREDICATE;
+      break;
+    case OR:
+      default_node_type = PREDICATE;
+      break;
+    case NOT:
+      default_node_type = PREDICATE;
+      break;
+    case EXPRESSION:
+      default_node_type = FUNCTION;
+      break;
+    case FUNCTION_MODIFIER:
+      default_node_type = FUNCTION;
+      break;
+  }
+  NodeType node_type = getNodeType(wexpr, default_node_type);
 
   if (wexpr == "(and)") {
     return nullptr;
@@ -375,10 +387,9 @@ std::shared_ptr<TreeNode> get_tree_node(const std::string & expr,
         std::shared_ptr<parser::pddl::tree::AndNode> pn_and = std::make_shared<parser::pddl::tree::AndNode>();
 
         std::vector<std::string> subexprs = getSubExpr(wexpr);
-        std::vector<std::string> construct_subexprs = getSubExpr(wconstruct);
 
-        for (unsigned i = 0; i < subexprs.size(); ++i ) {
-          pn_and->ops.push_back(get_tree_node(subexprs[i], construct_subexprs[i]));
+        for (unsigned i = 0; i < subexprs.size(); ++i) {
+          pn_and->ops.push_back(get_tree_node(subexprs[i], AND));
         }
 
         return pn_and;
@@ -388,10 +399,9 @@ std::shared_ptr<TreeNode> get_tree_node(const std::string & expr,
         std::shared_ptr<parser::pddl::tree::OrNode> pn_or = std::make_shared<parser::pddl::tree::OrNode>();
 
         std::vector<std::string> subexprs = getSubExpr(wexpr);
-        std::vector<std::string> construct_subexprs = getSubExpr(wconstruct);
 
-        for (unsigned i = 0; i < subexprs.size(); ++i ) {
-          pn_or->ops.push_back(get_tree_node(subexprs[i], construct_subexprs[i]));
+        for (unsigned i = 0; i < subexprs.size(); ++i) {
+          pn_or->ops.push_back(get_tree_node(subexprs[i], OR));
         }
 
         return pn_or;
@@ -401,9 +411,8 @@ std::shared_ptr<TreeNode> get_tree_node(const std::string & expr,
         std::shared_ptr<parser::pddl::tree::NotNode> pn_not = std::make_shared<parser::pddl::tree::NotNode>();
 
         std::vector<std::string> subexprs = getSubExpr(wexpr);
-        std::vector<std::string> construct_subexprs = getSubExpr(wconstruct);
 
-        pn_not->op = get_tree_node(subexprs[0], construct_subexprs[0]);
+        pn_not->op = get_tree_node(subexprs[0], NOT);
 
         return pn_not;
       }
@@ -435,10 +444,9 @@ std::shared_ptr<TreeNode> get_tree_node(const std::string & expr,
         expression->expr_type = getExprType(wexpr);
 
         std::vector<std::string> subexprs = getSubExpr(wexpr);
-        std::vector<std::string> construct_subexprs = getSubExpr(wconstruct);
 
-        for (unsigned i = 0; i < subexprs.size(); ++i ) {
-          expression->ops.push_back(get_tree_node(subexprs[i], construct_subexprs[i]));
+        for (unsigned i = 0; i < subexprs.size(); ++i) {
+          expression->ops.push_back(get_tree_node(subexprs[i], EXPRESSION));
         }
 
         return expression;
@@ -451,10 +459,9 @@ std::shared_ptr<TreeNode> get_tree_node(const std::string & expr,
         fun_mod->modifier_type = getFunModType(wexpr);
 
         std::vector<std::string> subexprs = getSubExpr(wexpr);
-        std::vector<std::string> construct_subexprs = getSubExpr(wconstruct);
 
-        for (unsigned i = 0; i < subexprs.size(); ++i ) {
-          fun_mod->ops.push_back(get_tree_node(subexprs[i], construct_subexprs[i]));
+        for (unsigned i = 0; i < subexprs.size(); ++i) {
+          fun_mod->ops.push_back(get_tree_node(subexprs[i], FUNCTION_MODIFIER));
         }
 
         return fun_mod;
