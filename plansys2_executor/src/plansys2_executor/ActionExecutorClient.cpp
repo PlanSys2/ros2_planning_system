2333b2bb54b53af173d4173b82fc94c3ad85cbe6
Jake Keller <jakeller3630@gmail.com>
2022-04-10 22:44:43 -0500

    Remove extra do_work() call in on_activate()
    do_work() may call the function finish(), which will fail to deactivate
    the node if it is still in the on_activate callback.

--- plansys2_executor/src/plansys2_executor/ActionExecutorClient.cpp
+++ plansys2_executor/src/plansys2_executor/ActionExecutorClient.cpp
@@ -101,7 +101,7 @@ ActionExecutorClient::on_activate(const rclcpp_lifecycle::State & state)
   timer_ = create_wall_timer(
     rate_, std::bind(&ActionExecutorClient::do_work, this));
 
-  do_work();
+//  do_work();
 
   return CallbackReturnT::SUCCESS;
 }
