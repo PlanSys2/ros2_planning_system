668b1388138283278f2ada96db10b5ce777358a7
Francisco Martín Rico <fmrico@gmail.com>
2025-12-14 08:21:53 +0100

    Revert "Support custom messages in BTAction"

--- plansys2_bt_actions/src/plansys2_bt_actions/BTAction.cpp
+++ plansys2_bt_actions/src/plansys2_bt_actions/BTAction.cpp
@@ -145,7 +145,6 @@ BTAction::do_work()
 {
   if (!finished_) {
     BT::NodeStatus result;
-    std::string out_msg;
     try {
       result = tree_.rootNode()->executeTick();
     } catch (BT::LogicError e) {
@@ -158,23 +157,16 @@ BTAction::do_work()
       finish(false, 0.0, "BTAction behavior tree threw an unknown exception");
     }
 
-    if (!blackboard_->get("out_msg", out_msg)) {
-      out_msg = "";
-    }
-
     switch (result) {
       case BT::NodeStatus::SUCCESS:
-        finish(true, 1.0,
-          out_msg.empty() ? "BTAction behavior tree returned SUCCESS" : out_msg);
+        finish(true, 1.0, "BTAction behavior tree returned SUCCESS");
         finished_ = true;
         break;
       case BT::NodeStatus::RUNNING:
-        send_feedback(0.0,
-          out_msg.empty() ? "BTAction behavior tree returned RUNNING" : out_msg);
+        send_feedback(0.0, "BTAction behavior tree returned RUNNING");
         break;
       case BT::NodeStatus::FAILURE:
-        finish(false, 1.0,
-          out_msg.empty() ? "BTAction behavior tree returned FAILURE" : out_msg);
+        finish(false, 1.0, "BTAction behavior tree returned FAILURE");
         finished_ = true;
         break;
     }
